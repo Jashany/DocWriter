e70219158fd2ba9f9fcab8beaf0f5bbbed6ab9b4
Akshi147 <itsaashi2004@gmail.com>
2024-01-30 16:25:34 +0530

    Backend and basic frontend done!

---
+++ backend/src/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Connection = async () => {
+  const URL = `mongodb+srv://akshita:akshi147@google-docs-clone.xo1p2ow.mongodb.net/?retryWrites=true&w=majority`;
+
+  try{
+    await mongoose.connect(URL);
+    console.log("Database connected successfully");
+  } catch(error){
+    console.log("Error while connecting to the database", error);
+  }
+}
+
+export default Connection;
